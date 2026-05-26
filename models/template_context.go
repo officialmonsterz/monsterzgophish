2306a4f66f787771977a26bbf1d354d3f925f7aa
Official Monsterz <willsmith32701@gmail.com>
2026-05-26 05:11:58 +0100

    Refactor URL handling and improve comments
    Updated comments and fixed query parameter setting.

--- models/template_context.go
+++ models/template_context.go
@@ -45,8 +45,7 @@ func NewPhishingTemplateContext(ctx TemplateContext, r BaseRecipient, rid string
         return PhishingTemplateContext{}, err
     }
 
-    // For the base URL, we'll reset the the path and the query
-    // This will create a URL in the form of http://example.com
+    // For the base URL, we'll reset the path and the query
     baseURL, err := url.Parse(templateURL)
     if err != nil {
         return PhishingTemplateContext{}, err
@@ -56,7 +55,7 @@ func NewPhishingTemplateContext(ctx TemplateContext, r BaseRecipient, rid string
 
     phishURL, _ := url.Parse(templateURL)
     q := phishURL.Query()
-    q.Set(RecipientParameter, rid)
+    q.Set("rid", rid)
     phishURL.RawQuery = q.Encode()
 
     trackingURL, _ := url.Parse(templateURL)
